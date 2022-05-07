14b3ef0887ad54d6b7904e0be98f74c0d1a78faf
unknown <alen2000james@gmail.com>
2022-05-07 16:08:23 -0400

    made solver and generator recursive so there is less time neede to find solution

--- src/componenets/SodukuBoard.java
+++ src/componenets/SodukuBoard.java
@@ -10,26 +10,37 @@ public class SodukuBoard {
         boolean set = false;
         ArrayList<Integer> possibilites = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9));
 
-        public int setValue(Random gen){
-            int poss = possibilites.size();
-            if(poss > 0){
-                value = possibilites.get(gen.nextInt(poss));
-                set = true;
-                return 0;
-            }
-            return -1;
+        Tile(){
+
         }
 
-        public void setValue(){
+        Tile(Tile tile){
+            this.value = tile.getValue();
+            this.set = tile.isSet();
+            this.possibilites = tile.getPossibilites();
+        }
+
+        public void setValue(int value){
+            set = true;
+            this.value = value;
+        }
+
+        public void resetValue(){
             value = 0;
         }
 
         public void removePossibility(int toRemove){
-            possibilites.remove((Integer)toRemove);
+            if(!set){
+                possibilites.remove((Integer)toRemove);
+            }
         }
 
         public int getValue(){
             return value;
+        }
+
+        public ArrayList<Integer> getPossibilites(){
+            return possibilites;
         }
 
         public int getPossibilitySize(){
@@ -51,8 +62,27 @@ public class SodukuBoard {
     }
 
     SodukuBoard(int remove){
-        createSolution();
+        populateBoard();
+        while(createSolution() != 0){
+            resetTiles();
+        };
         removeNumbers(remove);
+    }
+
+    SodukuBoard(SodukuBoard board) {
+        revert(board);
+    }
+
+    void revert(SodukuBoard board){
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                this.board[i][j] = new Tile(board.getBoard()[i][j]);
+            }
+        }
+    }
+
+    Tile[][] getBoard(){
+        return board;
     }
 
     void populateBoard(){
@@ -63,21 +93,32 @@ public class SodukuBoard {
         }
     }
 
-    void propagte(Integer[] location){
+    int propagte(Integer[] location){
         int toRemove = board[location[0]][location[1]].getValue();
         for(int i=0; i<9; i++){
             board[location[0]][i].removePossibility(toRemove);
+            if(board[location[0]][i].getPossibilitySize() == 0){
+                return -1;
+            }
         }
 
         for(int i=0; i<9; i++){
             board[i][location[1]].removePossibility(toRemove);
+            if(board[i][location[1]].getPossibilitySize() == 0){
+                return -1;
+            }
         }
 
         for(int i=0; i<3; i++){
             for(int j=0; j<3; j++){
                 board[location[0]/3*3+i][location[1]/3*3+j].removePossibility(toRemove);
+                if(board[location[0]/3*3+i][location[1]/3*3+j].getPossibilitySize() == 0){
+                    return -1;
+                }
             }
         }
+
+        return 0;
     }
 
     ArrayList<Integer[]> getLowestEntropy(){
@@ -94,6 +135,7 @@ public class SodukuBoard {
                         if(board[i][j].getPossibilitySize() < lowest){
                             toReturn.clear();
                             toReturn.add(new Integer[]{i,j});
+                            lowest = board[i][j].getPossibilitySize();
                         }
                         else if(board[i][j].getPossibilitySize() == lowest){
                             toReturn.add(new Integer[]{i,j});
@@ -105,33 +147,28 @@ public class SodukuBoard {
         return toReturn;
     }
 
-    public int addValues(int number){
-        for(int i=0; i<number; i++){
-            Random gen = new Random();
-            ArrayList<Integer[]> possibilites = getLowestEntropy();
-            Integer[] location = possibilites.get(gen.nextInt(possibilites.size()));
-            int success = board[location[0]][location[1]].setValue(gen);
-            if(success == -1){
-                return success;
-            }
-            propagte(location);
+    int createSolution(){
+        ArrayList<Integer[]> possibilites = getLowestEntropy();
+        Collections.shuffle(possibilites);
+        SodukuBoard temp = new SodukuBoard(this);
+        if(possibilites.size() == 0){
+            return 0;
         }
-        return 0;
-    }
-
-    void createSolution(){
-        boolean created = false;
-        while(!created){
-            created = true;
-            populateBoard();
-            Random gen = new Random();
-            Integer[] location = {gen.nextInt(board.length), gen.nextInt(board[0].length)};
-            board[location[0]][location[1]].setValue(gen);
-            propagte(location);
-            if(addValues(80) == -1){
-                created = false;
+        else {
+            for (int i = 0; i < possibilites.size(); i++) {
+                for (int k = 0; k < board[possibilites.get(i)[0]][possibilites.get(i)[1]].getPossibilites().size(); k++) {
+                    board[possibilites.get(i)[0]][possibilites.get(i)[1]].setValue(board[possibilites.get(i)[0]][possibilites.get(i)[1]].getPossibilites().get(k));
+                    if (propagte(possibilites.get(i)) == -1) {
+                        revert(temp);
+                    } else {
+                        if (createSolution() == 0) {
+                            return 0;
+                        }
+                    }
+                }
             }
         }
+        return -1;
     }
 
     void removeNumbers(int remove){
@@ -143,7 +180,7 @@ public class SodukuBoard {
             temp.add(gen.nextInt(board[0].length));
             if(!previous.contains(temp)){
                 previous.add(temp);
-                board[temp.get(0)][temp.get(1)].setValue();
+                board[temp.get(0)][temp.get(1)].resetValue();
             }
             else{
                 i--;
@@ -159,23 +196,22 @@ public class SodukuBoard {
         }
     }
 
-    int propagateSet(){
-        int sets = 0;
+    void propagateSet(){
         for(int i=0; i<9; i++){
             for(int j=0; j<9; j++){
                 if(board[i][j].getValue() != 0){
                     board[i][j].setSetTrue();
                     propagte(new Integer[]{i,j});
-                    sets++;
                 }
             }
         }
-        return sets;
     }
 
     public void solve(){
-        resetTiles();
-        while(addValues(81 - propagateSet()) != 0);
+        do{
+            resetTiles();
+            propagateSet();
+        }while(createSolution() != 0);
     }
 
     public void printBoard(){
--- src/componenets/main.java
+++ src/componenets/main.java
@@ -4,7 +4,7 @@ import componenets.SodukuBoard;
 
 public class main {
     public static void main(String args[]){
-        SodukuBoard myBoard = new SodukuBoard(30);
+        SodukuBoard myBoard = new SodukuBoard(80);
         myBoard.printBoard();
         myBoard.solve();
         System.out.println();
